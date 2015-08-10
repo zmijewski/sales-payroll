c5e2264587a50a2ef388c16845bc60a70ce3ddb8
Piotr Żmijewski <zmijewski.developer@gmail.com>
2015-08-10 18:52:51 +0200

    Added tests cases for PaymentDateCalculator

--- tests/PaymentDateCalculatorShould.php
+++ tests/PaymentDateCalculatorShould.php
@@ -14,4 +14,22 @@ class PaymentDateCalculatorShould extends \PHPUnit_Framework_TestCase
         $calculator = new PaymentDateCalculator(new \DateTime('2015-08-15'));
         $this->assertCount(5, $calculator->getPaymentDates());
     }
+
+    /**
+     * @test
+     */
+    public function returnOnePaymentDatesWhenDecemberPassed()
+    {
+        $calculator = new PaymentDateCalculator(new \DateTime('2015-12-15'));
+        $this->assertCount(1, $calculator->getPaymentDates());
+    }
+
+    /**
+     * @test
+     */
+    public function returnTwelvePaymentDatesWhenJanuaryPassed()
+    {
+        $calculator = new PaymentDateCalculator(new \DateTime('2015-01-15'));
+        $this->assertCount(12, $calculator->getPaymentDates());
+    }
 }
